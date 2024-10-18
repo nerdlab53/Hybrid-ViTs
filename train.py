a6afc73096148d9b4809ca673a3ee322ebc09607
nerdlab53 <retr0sushi.04@gmail.com>
2024-10-19 02:03:35 +0530

    added scheduler and training script

---
+++ train.py
@@ -0,0 +1,232 @@
+import argparse
+import os 
+import random
+import numpy as np 
+from datetime import timedelta
+
+import torch
+import torch.distributed as td
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from tqdm import tqdm 
+
+from models.VanillaViT import VanillaViT
+from models.VanillaViT_with_Inception import VanillaViT_with_Inception
+from models.VanillaViT_with_ModifiedInception import VanillaViT_with_ModifiedInception
+from utils.scheduler import WarmupCosineScheduler
+
+logger = logging.getLogger(__name__)
+
+class AverageMeter(Object):
+    """
+    Computes and stores the average and current value
+    """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def simple_accuracy(preds, labels):
+    return (preds==labels).mean()
+
+def save_model(args, model, global_step):
+    model_to_save = model.module if hasattr(model, 'module') else model
+    model_checkpoint = os.path.join(args.output_dir, f"{args.name}_checkpoint_{global_step}.bin")
+    torch.save(model_to_save.save_dict(), model_checkpoint)
+    logger.info("Saved model checkpoint to [DIR: %s]", args.output_dir)
+
+def setup(args):
+    if args.model_type == 'VanillaViT':
+        model = VanillaViT(num_classes=args.num_classes)
+    elif args.model_type == 'VanillaViT_with_Inception':
+        model = VanillaViT_with_Inception(num_classes=args.num_classes)
+    elif args.model_type == 'VanillaViT_with_ModifiedInception':
+        model = VanillaViT_with_ModifiedInception(num_classes=args.num_classes)
+    else:
+        raise ValueError(f"Unkown model type : {args.model_type}")
+
+    model.to(args.device)
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    logger.info("Total Parameters : \t%2.1fM" % (num_params / 1000000))
+
+    return args, model
+
+def set_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+def valid(args, model, writer, test_loader, global_step):
+    eval_loss = AverageMeter()
+
+
+    logger.info("***** Running Validation *****")
+    logger.info("  Num steps = %d", len(test_loader))
+    logger.info("  Batch size = %d", args.eval_batch_size)
+
+    model.eval()
+
+    all_preds, all_labels = [], []
+    epoch_iterator = tqdm(
+        test_loader,
+        desc = "Validating.. (loss=X.X)",
+        bar_format = "{l_bar}{r_bar}",
+        dynamic_ncols = True
+    )
+    loss_fct = torch.nn.CrossEntropyLoss()
+    for step, (x, y) in enumerate(epoch_iterator):
+        x = x.to(args.device)
+        y = y.to(args.device)
+
+        with torch.no_grad():
+            logits = model(x)
+            eval_loss = loss_fct(logits, y)
+            eval_losses.update(eval_loss.item())
+
+            preds = torch.argmax(logits, dim=-1)
+
+        if len(all_preds) == 0:
+            all_preds.append(preds.detach().cpu().numpy())
+            all_labels.append(y.detach().cpu().numpy())
+        else:
+            all_preds[0] = np.append(
+                all_preds[0], preds.detach().cpu().numpy(), axis=0
+            )
+            all_labels[0] = np.append(
+                all_labels[0], y.detach().cpu().numpy(), axis=0
+            )
+        epoch_iterator.set_description("Validation... (loss=%2.5f)" % eval_losses.val)
+
+    all_preds, all_labels = all_preds[0], all_lables[0]
+    accuracy = simple_accuracy(all_preds, all_labels)
+    logger.info("\n")
+    logger.info("Validation Results")
+    logger.info("Global Steps: %d" % global_step)
+    logger.info("Valid Loss: %2.5f" % eval_losses.avg)
+    logger.info("Valid Accuracy: %2.5f" % accuracy)
+
+    writer.add_scalar("test/accuracy", scalar_value=accuracy, global_step=global_step)
+    return accuracy
+
+def train(args, model):
+    """Training"""
+    if args.local_rank in [-1, 0]:
+        os.makedirs(args.output_dir, exist_ok=True)
+        writer = SummaryWriter(log_dir=os.path.join("logs", args.name))
+    
+    args.train_batch_size = args.train_batch_size // args.gradient_accumulation_steps
+    
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+    
+    transform_test = transforms.Compose([
+        transforms.Resize((args.img_size, args.img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+    
+    if args.dataset == "cifar10":
+        trainset = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform_train)
+        testset = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform_test)
+    
+
+    elif args.dataset == "cifar100":
+        trainset = datasets.CIFAR100(root="./data", train=True, download=True, transform=transform_train)
+        testset = datasets.CIFAR100(root="./data", train=False, download=True, transform=transform_test)
+
+    
+    train_loader = DataLoader(trainset, batch_size=args.train_batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    test_loader = DataLoader(testset, batch_size=args.eval_batch_size, shuffle=False, num_workers=4, pin_memory=True)
+
+    optimizer = torch.optim.SGD(model.parameters(),
+                                lr=args.learning_rate,
+                                momentum=0.9,
+                                weight_decay=args.weight_decay)
+    t_total = args.num_steps
+    
+    scheduler = WarmupCosineSchedule(optimizer, warmup_steps=args.warmup_steps, t_total=t_total)
+
+    logger.info("***** Running training *****")
+    logger.info("  Total optimization steps = %d", args.num_steps)
+    logger.info("  Instantaneous batch size per GPU = %d", args.train_batch_size)
+    logger.info("  Total train batch size (w. parallel, distributed & accumulation) = %d",
+                args.train_batch_size * args.gradient_accumulation_steps * (
+                    torch.distributed.get_world_size() if args.local_rank != -1 else 1))
+    logger.info("  Gradient Accumulation steps = %d", args.gradient_accumulation_steps)
+
+    model.zero_grad()
+    set_seed(args)
+    losses = AverageMeter()
+
+    global_step, best_acc = 0, 0
+
+    while True:
+        model.train()
+        epoch_iterator = tqdm(
+            train_loader,
+            desc="Training (X / X Steps) (loss=X.X)",
+            bar_format="{l_bar}{r_bar}",
+            dynamic_ncols=True
+        )
+
+        for step, (x, y) in enumerate(epoch_iterator):
+            x, y = x.to(args.device), y.to(args.device)
+            loss = model(x, y)
+
+            if args.gradient_accumulation_steps > 1:
+                loss = loss / args.gradient_accumulation_steps
+            loss.backward()
+
+            if (step + 1) % args.gradient_accumulation_steps == 0:
+                losses.update(loss.item()*args.gradient_accumulation_steps)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                scheduler.step()
+                optimizer.step()
+                optimizer.zero_grad()
+                global_step += 1
+
+                epoch_iterator.set_description(
+                    Training (%d / %d Steps) (loss=%2.5f)" % (global_step, t_total, losses.val)
+                )
+                if args.local_rank in [-1, 0]:
+                    writer.add_scalar("train/loss", scalar_value=losses.val, global_step=global_step)
+                    writer.add_scalar("train/lr", scalar_value=scheduler.get_lr()[0], global_step=global_step)
+                if global_step % args.eval_every == 0:
+                    accuracy = valid(args, model, writer, test_loader, global_step)
+                    if best_acc < accuracy:
+                        save_model(args, model, global_step)
+                        best_acc = accuracy
+                    model.train()
+
+                if global_step % t_total == 0:
+                    break
+        losses.reset()
+        if global_step % t_total == 0:
+            break
+
+    if args.local_rank in [-1, 0]:
+        writer.close()
+    logger.info("Best Accuracy: \t%f" % best_acc)
+    logger.info("End Training!")
+
+def main():
+    # Implement train.py further
+    pass
