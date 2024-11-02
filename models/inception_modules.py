e9d95e106aa8cc64bc3dbb9bccafb8166fe89da4
Himanshu Bhenwal <himanshubhenwal@Himanshus-MacBook-Air.local>
2024-11-02 23:23:34 +0530

    fixed weight initialization

--- models/inception_modules.py
+++ models/inception_modules.py
@@ -46,3 +46,58 @@ class InceptionModule(nn.Module):
         # Concatenate along channel dimension
         outputs = [branch1x1, branch3x3, branch5x5, branch_pool]
         return torch.cat(outputs, 1)
+
+class ModifiedInceptionModule(nn.Module):
+    def __init__(self, in_channels=3):
+        super().__init__()
+        
+        # Modified Branch 1: 1x1 conv with more filters
+        self.branch1x1_1 = nn.Conv2d(in_channels, 96, kernel_size=1)
+        
+        # Modified Branch 2: 1x1 conv -> 3x3 conv with increased channels
+        self.branch3x3_1 = nn.Conv2d(in_channels, 64, kernel_size=1)
+        self.branch3x3_2 = nn.Conv2d(64, 96, kernel_size=3, padding=1)
+        
+        # Modified Branch 3: 1x1 conv -> two 3x3 convs
+        self.branch3x3dbl_1 = nn.Conv2d(in_channels, 64, kernel_size=1)
+        self.branch3x3dbl_2 = nn.Conv2d(64, 96, kernel_size=3, padding=1)
+        self.branch3x3dbl_3 = nn.Conv2d(96, 96, kernel_size=3, padding=1)
+        
+        # Modified Branch 4: avg pool -> 1x1 conv
+        self.branch_pool = nn.AvgPool2d(kernel_size=3, stride=1, padding=1)
+        self.branch_pool_1 = nn.Conv2d(in_channels, 64, kernel_size=1)
+        
+        # Batch Normalization layers
+        self.bn1 = nn.BatchNorm2d(96)
+        self.bn2 = nn.BatchNorm2d(96)
+        self.bn3a = nn.BatchNorm2d(96)
+        self.bn3b = nn.BatchNorm2d(96)
+        self.bn4 = nn.BatchNorm2d(64)
+        
+        # Dropout for regularization
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        # Branch 1
+        branch1x1 = F.relu(self.bn1(self.branch1x1_1(x)))
+        branch1x1 = self.dropout(branch1x1)
+        
+        # Branch 2
+        branch3x3 = F.relu(self.branch3x3_1(x))
+        branch3x3 = F.relu(self.bn2(self.branch3x3_2(branch3x3)))
+        branch3x3 = self.dropout(branch3x3)
+        
+        # Branch 3
+        branch3x3dbl = F.relu(self.branch3x3dbl_1(x))
+        branch3x3dbl = F.relu(self.bn3a(self.branch3x3dbl_2(branch3x3dbl)))
+        branch3x3dbl = F.relu(self.bn3b(self.branch3x3dbl_3(branch3x3dbl)))
+        branch3x3dbl = self.dropout(branch3x3dbl)
+        
+        # Branch 4
+        branch_pool = self.branch_pool(x)
+        branch_pool = F.relu(self.bn4(self.branch_pool_1(branch_pool)))
+        branch_pool = self.dropout(branch_pool)
+        
+        # Concatenate along channel dimension
+        outputs = [branch1x1, branch3x3, branch3x3dbl, branch_pool]
+        return torch.cat(outputs, 1)
