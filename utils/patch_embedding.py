0e35c36ee3d2f96bee25306739bf11293ee6a5e3
Himanshu Bhenwal <himanshubhenwal@Himanshus-MacBook-Air.local>
2024-11-02 22:49:13 +0530

    fixed patch_embedding.py

--- utils/patch_embedding.py
+++ utils/patch_embedding.py
@@ -8,23 +8,23 @@ class PatchEmbedding(
 ):
     def __init__(self, in_channels:int = 3, patch_size: int = 16, embeddingdim: int = 768):
         super().__init__()
-        self.patch_size = 16
-        self.conv_patch_layer = nn.Conv2d(
+        self.patch_size = patch_size
+        self.projection = nn.Conv2d(
             in_channels=in_channels,
             out_channels=embeddingdim,
             kernel_size=patch_size,
             stride=patch_size,
             padding=0
         )
-        self.flatten = nn.flatten(start_dim=2, end_dim=3)
+        self.flatten = nn.Flatten(start_dim=2, end_dim=3)
 
     def forward(self, x):
 
         assert x.shape[-1] % self.patch_size == 0, f'Input Image is not divisible by patch size'
 
-        patches = self.conv_patch_layer(x)
+        patches = self.projection(x)
         flattened = self.flatten(patches)
-        return flattened.permute(0, 2, 1)
+        return flattened.transpose(1, 2)
         
         # After flattening, the shape comes out to be (batch, num_patches, embeddingdim)
 
